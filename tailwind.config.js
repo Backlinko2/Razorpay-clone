25c8860dbc78978ba117a2378d38bddbd442c989
Anuj <161225144+Backlinko2@users.noreply.github.com>
2024-08-27 17:29:09 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish: ["mullish","sans-serif"],
+      },
+      colors:{
+        deepBlue:"#02042a",
+        lightBlue:"#2b84ea",
+        greenLight:"#61cea6",
+        
+      }
+
+      
+    },
+  },
+  plugins: [],
+}
+
